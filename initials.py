6b7bd0e8e1bc170e70ab4f00f56a6d93d48b4f10
dotses444-hash <dotses444@gmail.com>
2026-05-31 11:38:04 +0000

    Add files via upload

---
+++ initials.py
@@ -0,0 +1,38 @@
+# File: <INITIALS>
+# Description: <TO PRINT MY INITIALS IN LARGE BLOCK LETTERS>
+# Assignment Number: <1>
+#
+# Name: <dOTSE SELORM>
+# STUDENT ID:  <2425402027>
+# Email: <dotses444@gmail.com>
+# Grader: <AUGUSTUS BUCKMAN>
+#
+# On my honor, <DOTSE SELORM>, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+
+
+
+
+def main():
+    
+    print()
+    
+    print("...MSD")
+    
+    print()
+
+    print("...MMMM....MMMM..........SSSSSSSSSS........DDDDDDDD.........")
+    print("...MMMM....MMMM.........SSSSSSSSSSS........DDDDDDDDDD.......")
+    print("...MM.MM..MM.MM........SSSS................DD.......DDD.....")
+    print("...MM.MM..MM.MM........SSSS................DD........DD.....")
+    print("...MM.MM..MM.MM..........SSSSSS............DD........DD.....")
+    print("...MM..MMMM..MM............SSSSSS..........DD........DD.....")
+    print("...MM..MMMM..MM................SSSS........DD........DD.....")
+    print("...MM...MM...MM................SSSS........DD........DD.....")
+    print("...MM...MM...MM...**...SSSSSSSSSSS....**...DDDDDDDDDD.....**")
+    print("...MM...MM...MM...**...SSSSSSSSSS.....**...DDDDDDDD.......**")
+    
+    print()
+main()
